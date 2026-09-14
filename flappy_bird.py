0e593b305b7fa1d68bca622a5c942b1bd4bb8d65
evans-au <nathan.evans75@det.nsw.edu.au>
2026-09-14 10:42:09 +1000

    Resources added to repo

---
+++ flappy_bird.py
@@ -0,0 +1,31 @@
+# start modules
+
+# create constants
+
+# print welcome
+
+# make background
+
+# make bird
+
+# make pipes
+
+# draw everything to screen
+
+    # draw background
+
+    # draw characters
+
+# update everything
+
+    # update bird
+
+    # update pipes
+
+    # bird hits bottom of screen
+
+    # bird hits pipes
+
+# moving
+
+# runs everything
